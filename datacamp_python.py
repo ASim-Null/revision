ba7e71424d7740b6e611678f061d8d302e9c415a
ASim-Null <51908131+ASim-Null@users.noreply.github.com>
2021-08-23 13:35:56 +0100

    Create datacamp_python.py

---
+++ datacamp_python.py
@@ -0,0 +1,8 @@
+# Remove fruits from basket2 that are present in basket1
+for item in basket1:
+    if item in basket2:
+        basket2.remove(item)
+
+print('Basket 1: ' + str(basket1))
+print('Basket 2: ' + str(basket2))
+
